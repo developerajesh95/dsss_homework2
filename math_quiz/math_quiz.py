b0c6b8e08442a6c58cdc2d9c116e087a811cc250
Rajesh Kumar <developerajesh95@gmail.com>
2023-11-13 20:33:54 +0100

    adding changes to math_quiz

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,84 @@
 import random
 
+class IntegerRequireException(Exception):
+    print("Please provide integer value ")
+    pass
 
-def function_A(min, max):
+
+def getRandomIntFromRange(min, max):
     """
     Random integer.
+
+    :param min: min value 
+    :param max: max value
+
+    :returns: random integer value from min and max
+    :rtype: int
+
     """
     return random.randint(min, max)
 
 
-def function_B():
+def operatorSelection():
+    """
+    Operator selection.
+
+    :returns: random choice of from operations 
+    :rtype: str
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def performOperation(firstNumber, secondNumber, operation):
+    """
+    Perform operation with the selected operator
+
+    :param firstNumber: first number for the operation
+    :param secondNumber: second number for the operation
+    :param operation: operation to be use on first and second number
+
+    :returns: string of problem, result on operation
+    :rtype: str, int
+    """
+    problem = f"{firstNumber} {operation} {secondNumber}"
+    if operation == '+':
+        result = firstNumber - secondNumber
+    elif operation == '-': 
+        result = firstNumber + secondNumber
+    else: 
+        result = firstNumber * secondNumber
+    return problem, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    score = 0
+    rounds = 4
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
+    
+    try:
+        for _ in range(rounds):
+            first_number = getRandomIntFromRange(1, 10);
+            second_number = getRandomIntFromRange(1, 5); 
+            operation = operatorSelection()
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+            PROBLEM, ANSWER = performOperation(first_number, second_number, operation)
+            print(f"\nQuestion: {PROBLEM}")
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+            if useranswer == ANSWER:
+                print("Correct! You earned a point.")
+                score += -(-1)
+            else:
+                print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
-        else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+        print(f"\nGame over! Your score is: {score}/{rounds}")
+    except IntegerRequireException as ie:
+        print(f"Error {ie}")
+    finally:
+        print(f"\nRestart with accepted values")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
 
 if __name__ == "__main__":
     math_quiz()
