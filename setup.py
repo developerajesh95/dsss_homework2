d156dacda4a9e6251d72b6645f5d9658ae6009df
Rajesh Kumar <developerajesh95@gmail.com>
2023-11-13 23:14:21 +0100

    changes in setup file

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(
-    name = "MathQuiz",
+    name = "math_quiz",
     version = "1.0.0",
+    py_modules = ['math_quiz'],
+    entry_points = {
+        'console_scripts':[
+            'math_quiz = math_quiz:main'
+        ]
+    },
+    install_requires=[],
     author = "Rajesh Kumar",
     author_email = "developerajesh95@gmail.com",
     packages = find_packages(),
